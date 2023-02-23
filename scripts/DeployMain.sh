8808361ca6ff8cb1a08b2e2adf8e55cfea42f61d
Rahul <rahulpatidar0191@gmail.com>
2023-02-23 13:19:52 -0800

    print inside main loop

--- scripts/DeployMain.sh
+++ scripts/DeployMain.sh
@@ -3,27 +3,28 @@
 function deploy_main_branch(){
   STORE_NAME=$1
   THEME_ID=$2 
-  THEMEKIT_PASSWORD=`grep -o '"'${STORE_NAME}'": "[^"]*' theme.json | grep -o '[^"]*$'`
+  echo "STORE_NAME=${STORE_NAME} THEME_ID=${THEME_ID}"
+  # THEMEKIT_PASSWORD=`grep -o '"'${STORE_NAME}'": "[^"]*' theme.json | grep -o '[^"]*$'`
 
-  #only change dir if theme files are in a different folder
-  if [[ -n $WORK_DIR ]] 
-  then
-      echo "WORK_DIR ${WORK_DIR}"
-      cd $WORK_DIR
-  fi 
+  # #only change dir if theme files are in a different folder
+  # if [[ -n $WORK_DIR ]] 
+  # then
+  #     echo "WORK_DIR ${WORK_DIR}"
+  #     cd $WORK_DIR
+  # fi 
 
-  theme configure --password=${THEMEKIT_PASSWORD} --store="${STORE_NAME}.myshopify.com" --themeid=${THEME_ID} --env ${THEME_ENV}
+  # theme configure --password=${THEMEKIT_PASSWORD} --store="${STORE_NAME}.myshopify.com" --themeid=${THEME_ID} --env ${THEME_ENV}
 
-  NAME=`TZ='US/Pacific' date`
-  NEW_THEME_NAME="${THEME_NAME^^}"
-  #This will rename the theme
-  curl -d "{\"theme\":{\"name\": \"${NEW_THEME_NAME} ${NAME}\", \"id\": \"${THEME_ID}\"}}" \
-        -X PUT "https://${STORE_NAME}.myshopify.com/admin/api/${SHOPIFY_API_VERSION}/themes/${THEME_ID}.json" \
-        -H "X-Shopify-Access-Token: ${THEMEKIT_PASSWORD}" \
-        -H "Content-Type: application/json" 
-  #Deploy to live
-  theme -e developtheme deploy --allow-live --ignored-file=config/settings_data.json    
-  cd ..
+  # NAME=`TZ='US/Pacific' date`
+  # NEW_THEME_NAME="${THEME_NAME^^}"
+  # #This will rename the theme
+  # curl -d "{\"theme\":{\"name\": \"${NEW_THEME_NAME} ${NAME}\", \"id\": \"${THEME_ID}\"}}" \
+  #       -X PUT "https://${STORE_NAME}.myshopify.com/admin/api/${SHOPIFY_API_VERSION}/themes/${THEME_ID}.json" \
+  #       -H "X-Shopify-Access-Token: ${THEMEKIT_PASSWORD}" \
+  #       -H "Content-Type: application/json" 
+  # #Deploy to live
+  # theme -e developtheme deploy --allow-live --ignored-file=config/settings_data.json    
+  # cd ..
 }
 
 
