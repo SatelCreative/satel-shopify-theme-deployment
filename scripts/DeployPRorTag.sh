b765f362868f9cc6207a851e92cf3fa603c19dbb
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2023-03-09 14:37:05 -0800

    print created theme id

--- scripts/DeployPRorTag.sh
+++ scripts/DeployPRorTag.sh
@@ -77,6 +77,7 @@ echo "====== Running deploy PR or Tag on store ${store} ====="
 deploy_pr_branch_or_tag "${store}"
 done 
 
+echo "THEME_ID=${THEME_ID}"
 # These outputs are used in other steps/jobs via action.yml
 echo "::set-output name=preview_link::${PREVIEW_LINKS[@]}" 
 echo "::set-output name=theme_id::${THEME_ID}" 
