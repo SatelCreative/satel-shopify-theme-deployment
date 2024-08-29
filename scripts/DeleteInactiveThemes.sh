1db460cbbdb0a269c735765d4ce26ef3d1f42966
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2024-08-29 09:42:27 -0700

    Add response code (#16)
    * add response
    
    * add response again
    
    * remove space

--- scripts/DeleteInactiveThemes.sh
+++ scripts/DeleteInactiveThemes.sh
@@ -15,15 +15,28 @@ function delete_inactive_themes() {
     do    
         if [[ ! "${BRANCH_NAMES[*]}" =~ "${THEME}" ]]; then
             echo "Themes that will be deleted PR:${THEME} on ${STORE_NAME}"
-            THEME_ID=`theme get --list --password=${THEMEKIT_PASSWORD} --store="${STORE_NAME}.myshopify.com" | grep -i ${THEME} | cut -d "[" -f 2 | cut -d "]" -f 1` # | cut -d "e" -f 2
+            THEME_ID=`theme get --list --password=${THEMEKIT_PASSWORD} --store="${STORE_NAME}.myshopify.com" | grep -i ${THEME} | cut -d "[" -f 2 | cut -d "]" -f 1`
     
-            curl -d "{\"theme\":{\"id\": \"${THEME_ID}\", \"name\": \"${THEME}\"}}" \
+            THEME=$(echo -n "${THEME}" | tr -d '[:space:]')
+            
+            RESPONSE=$(curl -s -w "\n%{http_code}" -d "{\"theme\":{\"id\": \"${THEME_ID}\",\"name\":\"${THEME}\"}}" \
             -X DELETE "https://${STORE_NAME}.myshopify.com/admin/api/${SHOPIFY_API_VERSION}/themes/${THEME_ID}.json" \
             -H "X-Shopify-Access-Token: ${THEMEKIT_PASSWORD}" \
-            -H "Content-Type: application/json"  
+            -H "Content-Type: application/json")
+            
+            RESPONSE_BODY=$(echo "$RESPONSE" | sed '$d')
+            HTTP_CODE=$(echo "$RESPONSE" | tail -n1)
+
+            if [[ $HTTP_CODE == "200" ]]; then
+                echo "Successfully deleted theme PR:${THEME} with ID:${THEME_ID} from ${STORE_NAME}"
+            else
+                echo "Failed to delete theme PR:${THEME} with ID:${THEME_ID} from ${STORE_NAME}. Response code: ${HTTP_CODE}"
+                echo "Response body: ${RESPONSE_BODY}"
+            fi
         fi
     done
 }
+
 function get_branch_list(){
     PAYLOAD="query { \
         organization(login: \\\"${ORG_NAME}\\\") {\
@@ -56,4 +69,4 @@ for store in "${stores[@]}"
 do
     echo "====== Running delete inactive themes on store ${store} ======"  
     delete_inactive_themes "${store}"
-done 
+done
