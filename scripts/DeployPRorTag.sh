b64662d8aaca767134d76c7f4abcdc69adb7decf
Rahul <rahulpatidar0191@gmail.com>
2024-11-13 14:05:06 -0800

    add cloning

--- scripts/DeployPRorTag.sh
+++ scripts/DeployPRorTag.sh
@@ -2,17 +2,15 @@
 PREVIEW_LINKS=()
 THEME_IDS=()
 
-deploy_pr_branch_or_tag() { 
+deploy_pr_branch_or_tag() {
 
-    STORE_NAME=$1
-    
-    if [[ -n $WORK_DIR ]] #only change dir if theme files are in a different folder than root
-    then
-        echo "WORK_DIR ${WORK_DIR}"
-        cd $WORK_DIR
-    fi  
-    
-    THEMEKIT_PASSWORD=`grep -E 'password:\s*.*' config.yml | sed 's/.*password:\s*//'`
+    STORE_NAME=$1 
+
+    mkdir temp
+    cd temp
+
+    # create theme
+        THEMEKIT_PASSWORD=`grep -E 'password:\s*.*' config.yml | sed 's/.*password:\s*//'`
     
     if [[ -n "${TAG_NAME}" ]]  
     then  
@@ -38,11 +36,11 @@ deploy_pr_branch_or_tag() {
         echo "Created theme id=${THEME_ID}"
     fi
 
+  # theme download
     if [[ $COPY_SETTINGS == true ]] && [[ -n $RUN_ID ]]; then   
         echo "Copy settings"
-        theme -e uat download  config/settings_data.json --live; STATUS1=$?
+        theme download --live; STATUS1=$?
     fi
-
 
     # Return the status code of theme commands
     TOTAL=$((STATUS1))
@@ -52,36 +50,49 @@ deploy_pr_branch_or_tag() {
        echo "Failing deployment 1"
        exit $TOTAL
     fi
-    sed -i "s/theme_id: THEME_ID/theme_id: ${THEME_ID}/" config.yml
-    
-    # echo "Generate PR preview links"
-    PREVIEW_LINK=`theme -e uat open -b /bin/echo | grep -i "${STORE_NAME}" | awk 'END {print \$3}'`
-    PREVIEW_LINKS+=( "Preview this PR on [${STORE_NAME}](${PREVIEW_LINK})<br>" )
 
-    echo "Running deploy command"
-    theme -e uat deploy; STATUS3=$?   
-    
-    THEME_IDS+=("${THEME_ID}")
-    
-    # To overcome first theme deploy's limitation for V2 of uploading files in a bad order, so deploy once again
-    if [[ $STATUS3 != 0 ]]
-    then 
-        echo "Re-deploying theme"
-        theme -e uat deploy; STATUS4=$?
-        if [[ $STATUS4 != 0 ]]
-        then 
-            # generate preview link even if it fails as the theme may have  gotten created, eg: Bondiboost
-            # These outputs are used in other steps/jobs via action.yml
-            echo "THEME_ID=${THEME_IDS[@]}"
-            echo "preview_link=${PREVIEW_LINKS[@]}" >> $GITHUB_OUTPUT
-            echo "theme_id=${THEME_IDS[@]}" >> $GITHUB_OUTPUT
+    # theme deploy
+    theme deploy
 
-            echo "Failing deployment 2"
-            exit $STATUS4 
-        fi  
-    fi   
-    cd .. # need to do this for next store
-}   
+    cd ..
+
+#     if [[ -n $WORK_DIR ]] #only change dir if theme files are in a different folder than root
+#     then
+#         echo "WORK_DIR ${WORK_DIR}"
+#         cd $WORK_DIR
+#     fi  
+    
+
+#     sed -i "s/theme_id: THEME_ID/theme_id: ${THEME_ID}/" config.yml
+    
+#     # echo "Generate PR preview links"
+#     PREVIEW_LINK=`theme -e uat open -b /bin/echo | grep -i "${STORE_NAME}" | awk 'END {print \$3}'`
+#     PREVIEW_LINKS+=( "Preview this PR on [${STORE_NAME}](${PREVIEW_LINK})<br>" )
+
+#     echo "Running deploy command"
+#     theme -e uat deploy; STATUS3=$?   
+    
+#     THEME_IDS+=("${THEME_ID}")
+    
+#     # To overcome first theme deploy's limitation for V2 of uploading files in a bad order, so deploy once again
+#     if [[ $STATUS3 != 0 ]]
+#     then 
+#         echo "Re-deploying theme"
+#         theme -e uat deploy; STATUS4=$?
+#         if [[ $STATUS4 != 0 ]]
+#         then 
+#             # generate preview link even if it fails as the theme may have  gotten created, eg: Bondiboost
+#             # These outputs are used in other steps/jobs via action.yml
+#             echo "THEME_ID=${THEME_IDS[@]}"
+#             echo "preview_link=${PREVIEW_LINKS[@]}" >> $GITHUB_OUTPUT
+#             echo "theme_id=${THEME_IDS[@]}" >> $GITHUB_OUTPUT
+
+#             echo "Failing deployment 2"
+#             exit $STATUS4 
+#         fi  
+#     fi   
+#     cd .. # need to do this for next store
+ }   
 
 
 stores=( ${STORE_NAME} )
