1dce22c39248be2ec45a3e029f8c551e36454d2b
Rahul <rahulpatidar0191@gmail.com>
2025-04-01 13:57:55 -0700

    fix typo

--- scripts/DeployPRorTag.sh
+++ scripts/DeployPRorTag.sh
@@ -25,9 +25,9 @@ deploy_pr_branch_or_tag() {
 
     # Generate PR preview link
     echo "===== Generating preview link ====="
-    PREVIEW_LINK=$(theme -e deploy open -b /bin/echo | grep -i "${STORE_NAME}" | awk 'END {print $3}')
-    theme -e deploy open -b /bin/echo | grep -i "${STORE_NAME}"
-    theme -e deploy open -b /bin/echo | grep -i "${STORE_NAME}" | awk 'END {print $3}'
+    PREVIEW_LINK=$(theme -e deployTheme open -b /bin/echo | grep -i "${STORE_NAME}" | awk 'END {print $3}')
+    theme -e deployTheme open -b /bin/echo | grep -i "${STORE_NAME}"
+    theme -e deployTheme open -b /bin/echo | grep -i "${STORE_NAME}" | awk 'END {print $3}'
     PREVIEW_LINKS+=("Preview this PR on [${STORE_NAME}](${PREVIEW_LINK})<br>")
 
     # Store theme ID
