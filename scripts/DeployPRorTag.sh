2ae94311ea0d215d8979d48beec40a43963ca224
Rahul <rahulpatidar0191@gmail.com>
2025-04-01 14:59:50 -0700

    clean up functions

--- scripts/DeployPRorTag.sh
+++ scripts/DeployPRorTag.sh
@@ -16,61 +16,42 @@ else
     echo "THEME_NAME: ${BRANCH_NAME}"
 fi
 
-
 deploy_pr_branch_or_tag() {
-    local STORE_NAME=$1 
+    local STORE_NAME="$1"
 
     # Get existing THEME_ID
     THEME_ID=$(theme get --list --password="${THEMEKIT_PASSWORD}" --store="${STORE_NAME}" | grep -i "${THEME_NAME}" | cut -d "[" -f 2 | cut -d "]" -f 1)
     echo "Existing THEME_ID=${THEME_ID}"
     
-    # Create temporary directory for theme cloning
-    mkdir -p temp
-    cp -r $WORK_DIR/* temp/
-    cd temp || exit
-
-    # # Clone the main theme for the first run before creating the new theme
-    echo "====== Cloning main theme to the new theme ====="
-    clone_published_theme "$STORE_NAME"
-
-    # Generate PR preview link
-    echo "===== Generating preview link ====="
-    PREVIEW_LINK=$(theme -e deployTheme --themeid="${THEME_ID}"  --password="${THEMEKIT_PASSWORD}" --store="${STORE_NAME}" open -b /bin/echo | grep -i "${STORE_NAME}" | awk 'END {print $3}')
-    PREVIEW_LINKS+=("Preview this PR on [${STORE_NAME}](${PREVIEW_LINK})<br>")
-
-    # Store theme ID
-    THEME_IDS+=("${THEME_ID}")
-
-    cd .. || exit  # Navigate back for the next store
-}
-
-clone_published_theme() {
-    local STORE_NAME=$1
-
+    # Create the theme if it doesn't exist
     if [[ -z "${THEME_ID}" ]]; then
-        # Create the theme if it doesn't exist
         echo "===== Creating theme ====="
         THEME_ID=$(curl -s -d "{\"theme\":{\"name\": \"PR: ${THEME_NAME}\", \"env\": \"${THEME_ENV}\"}}" \
             -X POST "https://${STORE_NAME}/admin/api/${SHOPIFY_API_VERSION}/themes.json" \
             -H "X-Shopify-Access-Token: ${THEMEKIT_PASSWORD}" \
             -H "Content-Type: application/json" | grep -o '"id":[0-9]*' | grep -o '[0-9]*')
-
-        echo "Created theme id=${THEME_ID}"
+        echo "Created theme ID=${THEME_ID}"
     fi
 
-    echo "===== Download theme stuff from live theme ====="
+    if [[ -n $WORK_DIR ]]; then  # Only change directory if theme files are in a different folder than root
+        echo "WORK_DIR: ${WORK_DIR}"
+        cd "$WORK_DIR" || exit
+    fi
+    echo "print ls"
+    ls
+
+    echo "===== Downloading theme settings from live theme ====="
     theme -e downloadPublishedSettings download --live
     STATUS1=$?
-
     if [[ $STATUS1 -ne 0 ]]; then
-        echo "Failing deployment due to download theme stuff from live theme failue"
+        echo "Failing deployment due to error in downloading live theme settings"
         exit $STATUS1
     fi
 
     # Update TARGET_THEME_ID in config.yml
     sed -i "s/theme_id: TARGET_THEME_ID/theme_id: ${THEME_ID}/" config.yml
 
-    echo "===== Deploying theme first time ====="
+    echo "===== Deploying theme for the first time ====="
     theme -e deployTheme deploy 
     STATUS2=$?
 
@@ -78,20 +59,23 @@ clone_published_theme() {
     if [[ $STATUS2 -ne 0 ]]; then
         echo "===== Re-deploying theme ====="
         theme -e deployTheme deploy 
-
         STATUS3=$?
         if [[ $STATUS3 -ne 0 ]]; then
-            # Generate preview link even if deployment fails
             echo "THEME_ID=${THEME_IDS[@]}"
             echo "preview_link=${PREVIEW_LINKS[@]}" >> "$GITHUB_OUTPUT"
             echo "theme_id=${THEME_IDS[@]}" >> "$GITHUB_OUTPUT"
-
-            echo "===== Failing deployment due to error in theme deploy ====="
+            echo "===== Failing deployment due to error in theme deployment ====="
             exit $STATUS3
         fi
     fi
 
-    cd .. || exit
+    # Generate PR preview link
+    echo "===== Generating preview link ====="
+    PREVIEW_LINK=$(theme -e deployTheme --themeid="${THEME_ID}" --password="${THEMEKIT_PASSWORD}" --store="${STORE_NAME}" open -b /bin/echo | grep -i "${STORE_NAME}" | awk 'END {print $3}')
+    PREVIEW_LINKS+=("Preview this PR on [${STORE_NAME}](${PREVIEW_LINK})<br>")
+    THEME_IDS+=("${THEME_ID}")
+
+    cd .. || exit 1  # Navigate back for the next store
 }
 
 # Iterate over stores and deploy the theme
