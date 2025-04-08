4f868b2fcfeffcb0d40d3fbd262f109ccbd1d0fe
Rahul <rahulpatidar0191@gmail.com>
2025-04-08 10:19:56 -0700

    check pwd

--- scripts/DeployMain.sh
+++ scripts/DeployMain.sh
@@ -11,27 +11,29 @@ function deploy_main_branch(){
     then
         echo "==== WORK_DIR ${WORK_DIR} ===="
         cd $WORK_DIR
+        pwd 
+        ls
     fi  
 
-    # Copy and update config
-    echo "==== Creating config.yml ===="
-    cp config.yml.example config.yml
-    sed -i "s/password: API_KEY/password: ${THEMEKIT_PASSWORD}/g" config.yml
-    sed -i "s/store: STORE/store: ${STORE_NAME}/g" config.yml
+    # # Copy and update config
+    # echo "==== Creating config.yml ===="
+    # cp config.yml.example config.yml
+    # sed -i "s/password: API_KEY/password: ${THEMEKIT_PASSWORD}/g" config.yml
+    # sed -i "s/store: STORE/store: ${STORE_NAME}/g" config.yml
     
     # LIST=`theme get --list --password=${THEMEKIT_PASSWORD} --store="${STORE_NAME}"`
     # echo "THEME LIST = ${LIST}"
 
-    # echo "===== Downloading theme settings from live theme ====="
+    # echo "===== Downloading theme settings from live theme on ${store} and MAIN THEME ID: ${id} ====="
     # theme -e downloadPublishedSettings download --live
     # STATUS1=$?
     # if [[ $STATUS1 -ne 0 ]]; then
-    #     echo "==== Failing deployment due to error in downloading live theme settings ===="
+    #     echo "==== Failing deployment due to error in downloading live theme settings on ${store} ===="
     #     exit $STATUS1
     # fi
 
-    sed -i "s/theme_id: TARGET_THEME_ID/theme_id: ${MAIN_THEME_ID}/" config.yml
-    cat config.yml
+    # sed -i "s/theme_id: TARGET_THEME_ID/theme_id: ${MAIN_THEME_ID}/" config.yml
+    # cat config.yml
     # if [[ -n $PRD_PARAMETER ]]; then
     #     PUBLISH_TEXT="DON'T PUBLISH "
     # fi
@@ -40,21 +42,21 @@ function deploy_main_branch(){
     # NEW_THEME_NAME="GITHUB-${BRANCH_NAME^^}"  # Add GitHub- prefix
 
     # # This will rename the theme
-    # echo "==== Rename theme ==== "
+    # echo "==== Rename theme on ${store} ==== "
     # curl -d "{\"theme\":{\"name\": \"${PUBLISH_TEXT}${NEW_THEME_NAME} ${TIME} \", \"id\": \"${MAIN_THEME_ID}\"}}" \
     #     -X PUT "https://${STORE_NAME}/admin/api/${SHOPIFY_API_VERSION}/themes/${MAIN_THEME_ID}.json" \
     #     -H "X-Shopify-Access-Token: ${THEMEKIT_PASSWORD}" \
     #     -H "Content-Type: application/json"
 
     # # Deploy to main theme on Shopify
-    # echo "==== Deploying to main theme on Shopify ==== "
+    # echo "==== Deploying to main theme on Shopify on ${store}==== "
     # theme -e deployTheme deploy --allow-live; STATUS1=$?    
 
     # # Return the status code of theme commands
     # TOTAL=$((STATUS1 + STATUS2))
     # if [[ $TOTAL != 0 ]]
     #     then 
-    #         echo "==== Failing deployment ===="
+    #         echo "==== Failing deployment on ${store} ===="
     #         exit $TOTAL
     #     fi 
 
